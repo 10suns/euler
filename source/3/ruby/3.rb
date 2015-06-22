b4c20ad95ed35ad9321889f510834049b7e2f01b
tranthanhan <tranthanhan@vn.oro.com>
2015-06-22 14:54:31 +0700

    update solution for problem 3.

--- source/3/ruby/3.rb
+++ source/3/ruby/3.rb
@@ -1,3 +1,14 @@
 require 'prime'
+n = 600_851_475_143
+i = 0
+#Solution 1
 answer = Prime.prime_division(600_851_475_143).flatten.at(-2)
 puts answer
+
+#Solution 2
+
+while n != 1 do
+  i += 1
+  n /= Prime.take(i).last if  n % Prime.take(i).last == 0
+end
+puts Prime.take(i).last
