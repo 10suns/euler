1da22ec81813fd8563b204ee0f85bcb8064d8e29
tranthanhan <tranthanhan@vn.oro.com>
2015-07-10 17:11:22 +0700

    refractor 4th solution.

--- source/4/ruby/4.rb
+++ source/4/ruby/4.rb
@@ -2,10 +2,10 @@ def is_palin?(number)
   number.to_s == number.to_s.reverse
 end
 
-palin_number = Array.new
+palin_number = 0
 (100..999).each do |i|
-  (100..999).each do |j| 
-    palin_number.push(i * j) if is_palin?(i * j)
+  (i..999).each do |j|
+    palin_number = (i * j) if is_palin?(i*j) && (i*j) > palin_number
   end
 end
-puts palin_number.sort.last
+puts palin_number
