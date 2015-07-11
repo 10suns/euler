d42cabce0b9bd3582f73e132ee53490d0f407d85
10_phoenix <tranthanhan0903@gmail.com>
2015-07-12 02:14:36 +0700

    modified solution 3rd

--- source/3/ruby/3.rb
+++ source/3/ruby/3.rb
@@ -1,13 +1,2 @@
 require 'prime'
-n = 600_851_475_143
-i = 0
-#Solution 1
-answer = Prime.prime_division(600_851_475_143).flatten.at(-2)
-puts answer
-
-#Solution 2
-while n != 1 do
-  i += 1
-  n /= Prime.take(i).last if  n % Prime.take(i).last == 0
-end
-puts Prime.take(i).last
+puts 600_851_475_143.prime_division(Prime::Generator23.new).flatten.max
