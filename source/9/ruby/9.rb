6cd661382144c3a0b15559fdff5545f514c46679
tranthanhan <tranthanhan@vn.oro.com>
2015-07-14 16:51:58 +0700

    refractor 9th, need a way to retrieve a,b,c with a+b+c=1000

--- source/9/ruby/9.rb
+++ source/9/ruby/9.rb
@@ -1,20 +1,11 @@
-#brutal force - we loop for every possible result
-found = false
-a = 0
-s = 1000
-while found == false do
-  a += 1
-  b = a + 1
-  while b < s / 2 do
-    c = s - a - b
-    if c**2 == a**2 + b**2 
-      found = true
-      break
+def pythagorean_triples
+  (1..Float::INFINITY).lazy.flat_map do |z|
+    (1..z).flat_map do |x|
+      (x..z).select do|y|
+        x**2 + y**2 == z**2
+      end.map do |y|
+        [x, y, z]
+      end
     end
-    b += 1
   end
 end
-
-puts a*b*c
-
-#another way
