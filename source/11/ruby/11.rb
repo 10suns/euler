b8c0b4936485c8b94edb821003d2d513f4c49a10
tranthanhan <tranthanhan@vn.oro.com>
2015-07-14 11:21:55 +0700

    refractor 11th, add method to create diagonal array

--- source/11/ruby/11.rb
+++ source/11/ruby/11.rb
@@ -19,6 +19,25 @@ flatten_grid = %w[08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48]
 
+class Array
+  def to_digonal_lines
+    result = []
+    (0...length).each do |i|
+      tmp1, tmp2 = [], []
+      m, n = 0, i
+      while n < length
+        tmp1 << self[m][n]
+        tmp2 << self[n][m] if i != 0
+        m += 1
+        n += 1
+      end
+      result << tmp1 if tmp1.any?
+      result << tmp2 if tmp2.any?
+    end
+    result
+  end
+end
+
 grid = []
 (0...20).each do |i|
   grid << flatten_grid[i*20, 20].map{|char| char.to_i}
@@ -37,13 +56,11 @@ def largest_production_of(array)
   max
 end
 
-def diagonal_array_of(array)
-
-end
-
 arr_production = [
   largest_production_of(grid),
-  largest_production_of(grid.transpose)
+  largest_production_of(grid.transpose),
+  largest_production_of(grid.to_digonal_lines),
+  largest_production_of(grid.map{|i| i.reverse}.to_digonal_lines)
 ]
 
 puts arr_production.max
