ace6fe056340f6c96de5a1a635b6dc063eea470f
tranthanhan <tranthanhan@vn.oro.com>
2015-07-23 14:10:06 +0700

    modify 14th solution.

--- source/14/ruby/14.rb
+++ source/14/ruby/14.rb
@@ -3,15 +3,9 @@ collatz_sequence = []
 (1..1e6).each do |n|
   tmp = [n]
   while tmp.last != 1
-    #tmp << tmp.last.even? ? tmp.last/2 : 3*tmp.last + 1
-
-    if tmp.last.even?
-      tmp << tmp.last/2
-    else
-      tmp << 3*tmp.last + 1
-    end
+    tmp << if tmp.last.even? then tmp.last/2 else 3*tmp.last + 1 end
   end
   collatz_sequence << tmp
 end
 
-p collatz_sequence.max_by{|a| a.length}.first
+puts collatz_sequence.max_by{|a| a.length}.first
