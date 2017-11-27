0d06051f0e348ad3dc0593a4a9d54941d88ce0d4
seven_suns <tranthanhan0903@gmail.com>
2017-11-27 09:25:48 +0000

    resolve 20th problem

--- source/20/ruby/20.rb
+++ source/20/ruby/20.rb
@@ -1,5 +1,5 @@
 def factorial_of(n)
-  n >1 ? n*factorial_of(n-1) : 1
+  n > 1 ? n*factorial_of(n-1) : 1
 end
 
-p factorial_of(100).to_s
+puts factorial_of(100).to_s.split('').inject(0) {|sum, n|sum + n.to_i}
