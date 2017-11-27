ef9a6cb33783fed397cceb5dc5d470d2b3c06de5
seven_suns <tranthanhan0903@gmail.com>
2017-11-27 08:54:15 +0000

    resolve problem 19th

---
+++ source/19/ruby/19.rb
@@ -0,0 +1,15 @@
+require 'time'
+
+start_date = Time.strptime("31/12/1899", "%d/%m/%Y")
+end_date = Time.strptime("31/12/2000", "%d/%m/%Y")
+timestamp_interval = 24 * 7 * 3600
+current_date = start_date
+count = 0;
+while(current_date <= end_date) do
+    if (current_date.day == 1 && current_date.year > 1900)
+        count += 1
+    end
+    current_date = current_date + timestamp_interval
+end
+
+puts count
